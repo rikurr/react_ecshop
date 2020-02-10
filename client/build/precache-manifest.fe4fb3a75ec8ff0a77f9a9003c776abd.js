306d1f82173a6542f36aa65db95e735917e22e7b
riku0329 user name <riku03290@gmail.com>
2020-02-11 01:12:15 +0900

    separating our app into client folder, added express server to help handle backend stripe payment

---
+++ client/build/precache-manifest.fe4fb3a75ec8ff0a77f9a9003c776abd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e41023dcb6c97311d0dc76ba375eb60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66b94316da2535054fae",
+    "url": "/static/css/main.09874cc7.chunk.css"
+  },
+  {
+    "revision": "c82fa785f619d4e9842b",
+    "url": "/static/js/2.ad92fd2b.chunk.js"
+  },
+  {
+    "revision": "66b94316da2535054fae",
+    "url": "/static/js/main.021ff1a9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
