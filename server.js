4b1ae124ec37e8a0cb0070663c3b7985d5ca4529
riku0329 user name <riku03290@gmail.com>
2020-02-26 13:13:09 +0900

    updata

--- server.js
+++ server.js
@@ -12,14 +12,12 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compresstion());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
-
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(compresstion());
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
 
